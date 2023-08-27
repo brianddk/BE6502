697d54dc8a25df92f79dac5fc10508d8084b6ffe
brianddk <brianddk@users.noreply.github.com>
2023-08-27 14:05:04 -0500

    Adding layout images and makerom.py modifications

--- 6502/src/makerom.py
+++ 6502/src/makerom.py
@@ -2,23 +2,32 @@
 # Please see this video for details:
 # https://www.youtube.com/watch?v=yl8vPW5hydQ
 #
+half_sec_delay = bytearray([0xea] * 250)
+
 code = bytearray([
-  0xa9, 0xff,         # lda #$ff
-  0x8d, 0x02, 0x60,   # sta $6002
+    0xa9, 0xff,         # lda #$ff
+    0x8d, 0x02, 0x60,   # sta $6002
+    
+    0xa9, 0x55,         # lda #$55
+    0x8d, 0x00, 0x60,   # sta $6000
+    
+    # Half Second Delay
+    ]) +  half_sec_delay + bytearray([
+    
+    0xa9, 0xaa,         # lda #$aa
+    0x8d, 0x00, 0x60,   # sta $6000
+    
+    # Half Second Delay
+    ]) + half_sec_delay + bytearray([
+    
+    0x4c, 0x05, 0x80,   # jmp $8005
+    ])
 
-  0xa9, 0x55,         # lda #$55
-  0x8d, 0x00, 0x60,   # sta $6000
-
-  0xa9, 0xaa,         # lda #$aa
-  0x8d, 0x00, 0x60,   # sta $6000
-
-  0x4c, 0x05, 0x80,   # jmp $8005
-  ])
-
-rom = code + bytearray([0xea] * (32768 - len(code)))
+rom = code + bytearray([0xea] * ((2 ** 15) - len(code)))
 
 rom[0x7ffc] = 0x00
 rom[0x7ffd] = 0x80
 
 with open("rom.bin", "wb") as out_file:
-  out_file.write(rom)
+    out_file.write(rom)
+    
